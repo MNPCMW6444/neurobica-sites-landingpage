8de645027c7e9f87d52d6c1b63849b6430997734
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-23 11:12:08 +0300

    awe

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import orange from "@mui/material/colors/orange";
 
@@ -8,14 +17,57 @@ const topbar = {
   top: "0px",
   margin: "0px",
   width: "100vw",
-  height: "10vh",
+  height: "7vh",
   backgroundColor: orange[100],
+};
+
+const menuButton = {
+  height: "7vh",
+  color: orange[900],
+  "&:hover": {
+    backgroundColor: orange[300],
+  },
 };
 
 function App() {
   return (
     <Box>
-      <Box sx={topbar}></Box>
+      <Box sx={topbar}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          wrap="nowrap"
+        >
+          <Grid item>logo</Grid>
+          <Grid
+            item
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            columnSpacing={2}
+          >
+            <Grid item>
+              <Button sx={menuButton}>Products</Button>
+            </Grid>
+            <Grid item>
+              <Button sx={menuButton}>Solutions</Button>
+            </Grid>
+            <Grid item>
+              <Button sx={menuButton}>Pricing</Button>
+            </Grid>
+            <Grid item>
+              <Button sx={menuButton}>Join Now</Button>
+            </Grid>
+            <Grid item>
+              <Button sx={menuButton}>Register</Button>
+            </Grid>
+          </Grid>
+          <Grid item>account</Grid>
+        </Grid>
+      </Box>
       <Grid
         container
         direction="column"
@@ -23,9 +75,34 @@ function App() {
         alignItems="center"
         spacing={1}
       >
-        <Typography variant="h1" component="h1">
-          Neurobica
-        </Typography>
+        <Grid item>
+          <Typography variant="h1" component="h1">
+            Neurobica
+          </Typography>
+        </Grid>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>asd</TableCell>
+              <TableCell>asd</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>asd</TableCell>
+              <TableCell>asd</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>asd</TableCell>
+              <TableCell>asd</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>asd</TableCell>
+              <TableCell>asd</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Grid item></Grid>
       </Grid>
     </Box>
   );
