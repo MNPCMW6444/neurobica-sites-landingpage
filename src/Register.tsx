0feefb0054f827d020e91f4929fae550e00d010c
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-24 00:07:54 +0300

    ad

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,12 +1,51 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import under from "./underline.svg";
+
+const veryNice = {
+  fontFamily: "proxima-nova, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+  letterSpacing: "-0.06rem",
+  fontWeight: "700",
+  lineHeight: "1",
+  color: "rgb(40, 40, 40)",
+  fontSize: "3.5rem",
+  boxsizing: "border-box",
+  position: "relative",
+  textAlign: "center",
+};
 
 export default function Register() {
   return (
-    <Box>
-      <Typography variant="h1" component="h1">
-        Neurobica
-      </Typography>
-    </Box>
+    <Grid
+      container
+      direction="column"
+      justify-content="flex-start"
+      alignItems="center"
+    >
+      <Grid item sx={veryNice}>
+        Everything you for
+      </Grid>
+      <Box
+        component="img"
+        src={under}
+        sx={{ width: "60%", alignSelf: "flex-start" }}
+      ></Box>
+      <Grid item sx={veryNice}>
+        Exercising you Brain
+      </Grid>
+      <Box
+        component="img"
+        src={under}
+        sx={{ width: "50%", alignSelf: "flex-start" }}
+      ></Box>
+      <Grid item sx={veryNice}>
+        In the same place
+      </Grid>
+      <Box
+        component="img"
+        src={under}
+        sx={{ width: "60%", alignSelf: "flex-end" }}
+      ></Box>
+    </Grid>
   );
 }
