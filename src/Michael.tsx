97cddc34f3c57e92254318c84e8d988fa2b6b6ef
YoadNatan <yoad12121223@gmail.com>
2022-09-23 22:31:34 +0300

    Update Michael.tsx

--- src/Michael.tsx
+++ src/Michael.tsx
@@ -3,34 +3,50 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
 
 export default function Michael() {
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>?</TableCell>
-          <TableCell>With Neurobica</TableCell>
-          <TableCell>Without Neurobica</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        <TableRow>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-          <TableCell>asd</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table" sx={{ alignItems: "center", alignContent: "center", justifyContent: "center" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>With Neurobica</TableCell>
+            <TableCell>Other Solutions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>Price</TableCell>
+            <TableCell>9.99$ a mounth/99.99$ a year + 150$ for </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Value</TableCell>
+            <TableCell>asd</TableCell>
+            <TableCell>asd</TableCell>
+
+          </TableRow>
+          <TableRow>
+            <TableCell>Time</TableCell>
+            <TableCell>asd</TableCell>
+            <TableCell>asd</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Comfort</TableCell>
+            <TableCell>asd</TableCell>
+            <TableCell>asd</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>asd</TableCell>
+            <TableCell>asd</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+
   );
 }
