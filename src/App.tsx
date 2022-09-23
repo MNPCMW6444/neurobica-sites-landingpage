7e06246fd30337bb9751e2c188b277d95903460d
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-23 22:00:53 +0300

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import orange from "@mui/material/colors/orange";
-
+import { useEffect, useState } from "react";
 import logo from "./logo512.png";
 import Michael from "./Michael";
+import Michaelforreal from "./Michaelforreal";
+import Mobile from "./Mobile";
+import Nav from "./Nav";
 
 const topbar = {
-  position: "static",
-  left: "0px",
-  top: "0px",
-  margin: "0px",
+  backgroundColor: orange[100],
   width: "100vw",
   height: "50px",
-  backgroundColor: orange[100],
-};
-
-const menuButton = {
-  height: "50px",
-  color: orange[900],
-  "&:hover": {
-    backgroundColor: orange[300],
-  },
 };
 
 const logoSx = {
@@ -29,67 +20,47 @@ const logoSx = {
   width: "50px",
 };
 
-const accountSx = {
-  paddingRight: "10px",
-};
-
 function App() {
+  const [ismobile, setismobile] = useState(window.innerWidth < 600);
+  useEffect(() => {
+    const handleResize = () => {
+      setismobile(window.innerWidth < 600);
+    };
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
-    <Box>
-      <Box sx={topbar}>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          wrap="nowrap"
-        >
-          <Grid item>
-            <Box component="img" src={logo} sx={logoSx}></Box>
-          </Grid>
-          <Grid
-            item
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            columnSpacing={2}
-          >
-            <Grid item>
-              <Button sx={menuButton}>Products</Button>
-            </Grid>
-            <Grid item>
-              <Button sx={menuButton}>Solutions</Button>
-            </Grid>
-            <Grid item>
-              <Button sx={menuButton}>Pricing</Button>
-            </Grid>
-            <Grid item>
-              <Button sx={menuButton}>Join Now</Button>
-            </Grid>
-            <Grid item>
-              <Button sx={menuButton}>Register</Button>
-            </Grid>
-          </Grid>
-          <Grid item>account</Grid>
-        </Grid>
-      </Box>
-      <Grid
-        container
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="center"
-        spacing={1}
-      >
-        <Grid item>
-          <Typography variant="h1" component="h1">
-            Neurobica
-          </Typography>
-        </Grid>
-        <Michael />
-        <Grid item></Grid>
-      </Grid>
-    </Box>
+    <div style={{ overflowY: "scroll" }}>
+      <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p> <p>asdas</p>
+      <p>asdas</p>
+      <p>asdas</p>
+    </div>
   );
 }
 
