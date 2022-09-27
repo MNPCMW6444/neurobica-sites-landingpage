9d651fc776f20b0890cab21f03bed224c377c6de
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-27 15:52:55 +0300

    Update Features.tsx

--- src/Features.tsx
+++ src/Features.tsx
@@ -1,17 +1,7 @@
-import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Avatar } from "@mui/material";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 export default function RowAndColumnSpacing() {
   return (
