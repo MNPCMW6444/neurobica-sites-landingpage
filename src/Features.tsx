511e0c8b09d3ea90d6b74abd968e13fbfc3adac9
YoadNatan <yoad12121223@gmail.com>
2022-09-27 17:21:21 +0300

    feat

--- src/Features.tsx
+++ src/Features.tsx
@@ -5,7 +5,10 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Avatar } from '@mui/material';
-
+import memory from "./memory.png"
+import attention from "./attention2.png"
+import multiplayer from "./multiplayer.png"
+import performance from ".//performance.png"
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
@@ -27,12 +30,12 @@ export default function RowAndColumnSpacing() {
                 direction="column"
             >
                 <Grid item xs={12} md={12} lg={12}>
-                    <Typography align="center" sx={{ padding: "2vh", maxWidth: "70vW" }} variant="h4">
+                    <Typography align="center" sx={{ padding: "2vh", maxWidth: "70vW", fontSize: "7vw" }} variant="h4">
                         If we didn't change your mind - you can!
                     </Typography>
                 </Grid>
                 <Grid item xs={12} md={12} lg={12} height="100vhs">
-                    <Typography align="center" variant="h5" >
+                    <Typography sx={{ fontSize: "3vw" }} align="center" variant="h5" >
                         100% money back guarantee for 30 days!
                     </Typography>
                     <br></br>
@@ -44,25 +47,33 @@ export default function RowAndColumnSpacing() {
                 width="100vw"
                 alignItems="center"
                 direction="row" sx={{ padding: "5vh" }} rowSpacing={{ xs: 1, sm: 2, md: 2 }} columnSpacing={{ xs: 3, sm: 2, md: 2 }}>
-                <Grid item xs={6} width="35vh" height="30vh" md={3}>
-                    <Avatar sx={{ width: 75, height: 75 }}></Avatar>
-                    <Typography align='center'> Attention and focus neurofeedback training</Typography>
+                <Grid item xs={6} width="35vh" height="30vh" md={3} sx={{ maxWidth: "40vw" }}>
+                    <Avatar alt="attention" src={attention} sx={{ width: "10vh", height: "10vh" }}></Avatar>
+                    <Typography sx={{ fontSize: "3vh" }} align='center'> Attention and focus neurofeedback training</Typography>
                 </Grid>
-                <Grid item xs={6} width="35vh" height="30vh" md={3}>
-                    <Avatar
-                        sx={{ width: 75, height: 75 }}></Avatar>
-                    <Typography align='center'> Memory neurofeedback training</Typography>
+                <Grid item xs={6} width="35vh" height="30vh" md={3} alignItems="center" justifyContent="center">
+                    <Grid item>
+                        <Avatar alt="memory"
+                            src={memory}
+                            sx={{ width: "10vh", height: "10vh" }}></Avatar>
+                    </Grid>
+                    <Grid item>
+
+                        <Typography sx={{ fontSize: "3vh" }} align='center'> Memory neurofeedback training</Typography>
+                    </Grid>
+                </Grid>
+
+                <Grid item width="35vh" height="30vh" xs={6} md={3}>
+                    <Avatar src={multiplayer}
+                        sx={{ width: "10vh", height: "10vh" }}></Avatar>
+                    <Typography sx={{ fontSize: "3vh" }} align='center'>The first and only online multyplayer neurofeedback training platform</Typography>
                 </Grid>
                 <Grid item width="35vh" height="30vh" xs={6} md={3}>
-                    <Avatar
-                        sx={{ width: 75, height: 75 }}></Avatar>
-                    <Typography align='center'>The first and only online multyplayer neurofeedback training platform</Typography>
+                    <Avatar src={performance} alt="performance"
+                        sx={{ width: "10vh", height: "10vh" }}></Avatar>
+                    <Typography sx={{ fontSize: "3vh" }} align='center'>  Science proved brain prformence assasment to check your progress</Typography>
                 </Grid>
-                <Grid item width="35vh" height="30vh" xs={6} md={3}>
-                    <Avatar
-                        sx={{ width: 75, height: 75 }}></Avatar>
-                    <Typography align='center'>  Science proved brain prformence assasment to check your progress</Typography>
-                </Grid>
+
             </Grid>
 
         </Box >
